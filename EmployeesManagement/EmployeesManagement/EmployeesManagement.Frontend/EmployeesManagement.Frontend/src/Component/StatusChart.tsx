104d3434a212c4a3e5a2f95cd392bf45180862ec
saadbenboujina <saadbenboujina@gmail.com>
2024-06-11 18:31:21 +0200

    .

--- EmployeesManagement/EmployeesManagement/EmployeesManagement.Frontend/EmployeesManagement.Frontend/src/Component/StatusChart.tsx
+++ EmployeesManagement/EmployeesManagement/EmployeesManagement.Frontend/EmployeesManagement.Frontend/src/Component/StatusChart.tsx
@@ -2,28 +2,40 @@ import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { countBy } from "lodash";
 import { Status } from "../generatedCode/src/generatedCode/generated"; // Angepasster Pfad
+import { ChartData, ChartDataset } from "chart.js";
 
 interface IStatusChartProps {
   data: Status[];
 }
 
-const StatusChart: React.FC<IStatusChartProps> = ({ data }) => {
+function StatusChart(props: IStatusChartProps) {
+  const  data  = props;
   const statusGrouped = countBy(data);
-  const values = Object.keys(statusGrouped).map(key => {
-    const statusKey = Number(key) as Status;
-    return {
-      key: (Status[statusKey]), // Assuming Status enum has keys like "_0", "_1"
+  const values = [];
+  const Colors: { [key: string]: string } = {
+    Active: "green",
+    Inactive: "red",
+  };
+    for (const key of Object.keys(statusGrouped)) {
+    const label = key;
+    const typedColorString = label as keyof typeof Colors;
+    values.push({
+      key: key,
       value: statusGrouped[key],
-      color: statusKey === Status._0 ? "red" : "green" // Colors based on status value
-    };
-  });
+      color: Colors[typedColorString],
+    });
+  }
+  const dataSet: ChartDataset<"doughnut", number[]> = {
+    label: "Status",
+    backgroundColor: values.map((x) => x.color),
+    data: values.map((x) => x.value),
+  };
 
-  const chartData = {
-    labels: values.map(v => v.key),
-    datasets: [{
-      data: values.map(v => v.value),
-      backgroundColor: values.map(v => v.color)
-    }]
+  const dataSets: ChartDataset<"doughnut", number[]>[] = [];
+  dataSets.push(dataSet);
+  const chartData: ChartData<"doughnut"> = {
+    labels: values.map((x) => x.key),
+    datasets: dataSets,
   };
 
   const options = {
@@ -38,7 +50,23 @@ const StatusChart: React.FC<IStatusChartProps> = ({ data }) => {
     return <em>{("No data to display")}</em>;
   }
 
-  return <Doughnut data={chartData} options={options} />;
-};
+  return (
+    <>
+      <Doughnut data={chartData} options={options} />
+      { 
+        <div className="d-flex justify-content-around mb-0 mt-3">
+          {values.map((x) => (
+            <div key={x.key}>
+              {(x.key)}
+              <h3 className="font-weight-bold" style={{ color: x.color }}>
+                {x.value}
+              </h3>
+            </div>
+          ))}
+        </div>
+      }
+    </>
+  );
+}
 
 export default StatusChart;
