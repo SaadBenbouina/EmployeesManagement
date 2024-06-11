dc648f78db8e1dbcf72770b4845f0069fb94deb2
saadbenboujina <saadbenboujina@gmail.com>
2024-06-11 14:06:13 +0200

    ,

--- EmployeesManagement/EmployeesManagement/EmployeesManagement.Frontend/EmployeesManagement.Frontend/src/Pages/HomePage.tsx
+++ EmployeesManagement/EmployeesManagement/EmployeesManagement.Frontend/EmployeesManagement.Frontend/src/Pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import './App.css';
+import '../App.css';
 import CardViewHomePage from '../CardViewHomepage';
 import TitleComponent from '../TitleComponent';
 import { FaUsersCog } from 'react-icons/fa';
